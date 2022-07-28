b665725707a8432fb6bac5bfc5634d3de7f4b465
Sigit Prayitno <sigit@asetku.com>
2022-07-28 22:58:42 +0700

    Initiate Spring Boot Persistence

---
+++ spring-boot-persistence/spring-boot-jpa-mysql/src/main/java/com/coffeegit/springboot/jpamysql/SpringBootJpaMysqlApp.java
@@ -0,0 +1,13 @@
+package com.coffeegit.springboot.jpamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaMysqlApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaMysqlApp.class, args);
+	}
+
+}
---
+++ spring-boot-persistence/spring-boot-jpa-mysql/src/test/java/com/coffeegit/springboot/jpamysql/SpringBootJpaMysqlAppTests.java
@@ -0,0 +1,13 @@
+package com.coffeegit.springboot.jpamysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaMysqlAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
